3988b22fbab4c8528f39bc35ffebc936cc3d62ba
PamelaKlapp <pameklapp@gmail.com>
2022-12-06 11:56:56 +0100

    change render() for createRoot()

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
-import reactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-reactDOM.render(<App />, document.querySelector('#root'));
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+root.render(
+    <App/>
+)
