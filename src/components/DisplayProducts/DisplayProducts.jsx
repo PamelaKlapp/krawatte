6fa1214169e2612c41ca7288361a9afeb16e7db2
PamelaKlapp <pameklapp@gmail.com>
2022-12-09 10:31:40 +0100

    Update DisplayProducts.jsx

--- src/components/DisplayProducts/DisplayProducts.jsx
+++ src/components/DisplayProducts/DisplayProducts.jsx
@@ -1,21 +1,20 @@
-import React from 'react'
+import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import Img from '../../assets/krawatte2.png';
-import "swiper/css";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
-
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 
 // import required modules
-import { Pagination, Navigation } from "swiper";
+import { Pagination, Navigation } from 'swiper';
 
-import './DisplayProducts.css'
+import './DisplayProducts.css';
 
 const DisplayProducts = () => {
   return (
-    <div className='displayP-container'>
-        <div></div>
-        <Swiper
+    <div className="displayP-container">
+      <div></div>
+      <Swiper
         slidesPerView={3}
         spaceBetween={30}
         slidesPerGroup={3}
@@ -28,97 +27,112 @@ const DisplayProducts = () => {
         modules={[Pagination, Navigation]}
         className="mySwiper"
       >
-        <SwiperSlide><article>
-        <div className="image">
-          <img src={Img} alt="" />
-        </div>
-        <div className="product-info">
-          <h5>Krawatte Le Wein</h5>
-          <a href="">Mehr..</a>
-        </div>
-      </article></SwiperSlide>
-        <SwiperSlide><article>
-        <div className="image">
-          <img src={Img} alt="" />
-        </div>
-        <div className="product-info">
-          <h5>Krawatte Le Wein</h5>
-          <a href="">Mehr..</a>
-        </div>
-      </article></SwiperSlide>
-        <SwiperSlide><article>
-        <div className="image">
-          <img src={Img} alt="" />
-        </div>
-        <div className="product-info">
-          <h5>Krawatte Le Wein</h5>
-          <a href="">Mehr..</a>
-        </div>
-      </article></SwiperSlide>
-        <SwiperSlide><article>
-        <div className="image">
-          <img src={Img} alt="" />
-        </div>
-        <div className="product-info">
-          <h5>Krawatte Le Wein</h5>
-          <a href="">Mehr..</a>
-        </div>
-      </article></SwiperSlide>
-      
-        <SwiperSlide><article>
-        <div className="image">
-          <img src={Img} alt="" />
-        </div>
-        <div className="product-info">
-          <h5>Krawatte Le Wein</h5>
-          <a href="">Mehr..</a>
-        </div>
-      </article></SwiperSlide>
-        <SwiperSlide><article>
-        <div className="image">
-          <img src={Img} alt="" />
-        </div>
-        <div className="product-info">
-          <h5>Krawatte Le Wein</h5>
-          <a href="">Mehr..</a>
-        </div>
-      </article></SwiperSlide>
-        <SwiperSlide><article>
-        <div className="image">
-          <img src={Img} alt="" />
-        </div>
-        <div className="product-info">
-          <h5>Krawatte Le Wein</h5>
-          <a href="">Mehr..</a>
-        </div>
-      </article></SwiperSlide>
-        <SwiperSlide><article>
-        <div className="image">
-          <img src={Img} alt="" />
-        </div>
-        <div className="product-info">
-          <h5>Krawatte Le Wein</h5>
-          <a href="">Mehr..</a>
-        </div>
-      </article></SwiperSlide>
-        <SwiperSlide><article>
-        <div className="image">
-          <img src={Img} alt="" />
-        </div>
-        <div className="product-info">
-          <h5>Krawatte Le Wein</h5>
-          <a href="">Mehr..</a>
-        </div>
-      </article></SwiperSlide>
+        <SwiperSlide>
+          <article>
+            <div className="image">
+              <img src={Img} alt="" />
+            </div>
+            <div className="product-info">
+              <h4>Krawatte Le Wein</h4>
+              <br />
+              <a href="">Mehr..</a>
+            </div>
+          </article>
+        </SwiperSlide>
+        <SwiperSlide>
+          <article>
+            <div className="image">
+              <img src={Img} alt="" />
+            </div>
+            <div className="product-info">
+              <h4>Krawatte Le Wein</h4>
+              <br />
+              <a href="">Mehr..</a>
+            </div>
+          </article>
+        </SwiperSlide>
+        <SwiperSlide>
+          <article>
+            <div className="image">
+              <img src={Img} alt="" />
+            </div>
+            <div className="product-info">
+              <h5>Krawatte Le Wein</h5>
+              <a href="">Mehr..</a>
+            </div>
+          </article>
+        </SwiperSlide>
+        <SwiperSlide>
+          <article>
+            <div className="image">
+              <img src={Img} alt="" />
+            </div>
+            <div className="product-info">
+              <h5>Krawatte Le Wein</h5>
+              <a href="">Mehr..</a>
+            </div>
+          </article>
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <article>
+            <div className="image">
+              <img src={Img} alt="" />
+            </div>
+            <div className="product-info">
+              <h5>Krawatte Le Wein</h5>
+              <a href="">Mehr..</a>
+            </div>
+          </article>
+        </SwiperSlide>
+        <SwiperSlide>
+          <article>
+            <div className="image">
+              <img src={Img} alt="" />
+            </div>
+            <div className="product-info">
+              <h5>Krawatte Le Wein</h5>
+              <a href="">Mehr..</a>
+            </div>
+          </article>
+        </SwiperSlide>
+        <SwiperSlide>
+          <article>
+            <div className="image">
+              <img src={Img} alt="" />
+            </div>
+            <div className="product-info">
+              <h5>Krawatte Le Wein</h5>
+              <a href="">Mehr..</a>
+            </div>
+          </article>
+        </SwiperSlide>
+        <SwiperSlide>
+          <article>
+            <div className="image">
+              <img src={Img} alt="" />
+            </div>
+            <div className="product-info">
+              <h5>Krawatte Le Wein</h5>
+              <a href="">Mehr..</a>
+            </div>
+          </article>
+        </SwiperSlide>
+        <SwiperSlide>
+          <article>
+            <div className="image">
+              <img src={Img} alt="" />
+            </div>
+            <div className="product-info">
+              <h5>Krawatte Le Wein</h5>
+              <a href="">Mehr..</a>
+            </div>
+          </article>
+        </SwiperSlide>
       </Swiper>
       <div></div>
-
     </div>
+  );
+};
 
-    
-  )
-
-  
-}
-
-export default DisplayProducts
+export default DisplayProducts;
