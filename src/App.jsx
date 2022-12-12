804119f4f8c6a919dbd0fa27dfcfdc95d42d8cf8
PamelaKlapp <pameklapp@gmail.com>
2022-12-12 17:28:21 +0100

    Add Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar/Navbar'
 import Products from './components/Products/Products'
 import Company from './components/Company/Company'
 import FAQs from './components/FAQs/FAQs'
+import Footer from './components/Footer/Footer'
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
    <Products/>
    <Company/>
    <FAQs/>
+   <Footer/>
    </>
 
     
