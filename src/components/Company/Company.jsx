dfc95704215dc388e1c7ffd79d315b897f8f9c53
PamelaKlapp <pameklapp@gmail.com>
2022-12-16 14:44:43 +0100

    Update Company.jsx

--- src/components/Company/Company.jsx
+++ src/components/Company/Company.jsx
@@ -4,29 +4,33 @@ import CompanyImg from '../../assets/krawatte7.png';
 
 const Company = () => {
   return (
-    <div id='company' className="company_container container">
-      <div className="left-side">
+    <section id="company" className="company_container container">
+      <div className="company_title">
         <h4>Wir sind..</h4>
         <h2>Die neue Ära der Mode</h2>
-        <div className="company-img">
-          <img src={CompanyImg} alt="" />
+      </div>
+      <div className="content">
+        <div className="left-side">
+          <div className="company-img">
+            <img src={CompanyImg} alt="" />
+          </div>
+        </div>
+        <div className="right-side">
+          <p>
+            Unsere Produkte werden unter fairen Handelsbedingungen hergestellt.
+            Wir sorgen für das Wohlergehen und die guten Beziehungen unseres
+            Teams und unserer Lieferanten. Wir setzen uns für eine Arbeitskultur
+            ein, die zu einer besseren Lebensqualität für uns alle beiträgt. Wir
+            arbeiten vor allem mit hoch qualifizierten lokalen Produzenten
+            zusammen und stellen sicher, dass wir sie fair bezahlen. Bei der
+            Herstellung eines jeden Produkts gibt es einen Designprozess, der
+            sich mit diesen Idealen auseinandersetzt, indem er eine
+            Verschmelzung von Materialien, Prozessen, Lieferanten und Menschen
+            anstrebt, um das ideale Produkt zu erreichen.{' '}
+          </p>
         </div>
       </div>
-      <div className="right-side">
-        <p>
-          Unsere Produkte werden unter fairen Handelsbedingungen hergestellt.
-          Wir sorgen für das Wohlergehen und die guten Beziehungen unseres Teams
-          und unserer Lieferanten. Wir setzen uns für eine Arbeitskultur ein,
-          die zu einer besseren Lebensqualität für uns alle beiträgt. Wir
-          arbeiten vor allem mit hoch qualifizierten lokalen Produzenten
-          zusammen und stellen sicher, dass wir sie fair bezahlen. Bei der
-          Herstellung eines jeden Produkts gibt es einen Designprozess, der sich
-          mit diesen Idealen auseinandersetzt, indem er eine Verschmelzung von
-          Materialien, Prozessen, Lieferanten und Menschen anstrebt, um das
-          ideale Produkt zu erreichen.{' '}
-        </p>
-      </div>
-    </div>
+    </section>
   );
 };
 
