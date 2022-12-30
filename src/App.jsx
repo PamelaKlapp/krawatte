569fbcceef75871be292ee0613e00e5d92dbef10
PamelaKlapp <pameklapp@gmail.com>
2022-12-30 12:01:47 +0100

    re-design css

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,29 @@
-import React from 'react'
-import Header from './components/Header/Header'
-import Navbar from './components/Navbar/Navbar'
-import Products from './components/Products/Products'
-import Company from './components/Company/Company'
-import FAQs from './components/FAQs/FAQs'
-import Footer from './components/Footer/Footer'
+import React from 'react';
+import Home from './components/Home/Home';
+import Navbar from './components/Navbar/Navbar';
+import Products from './components/Products/Products';
+import Company from './components/Company/Company';
+import FAQs from './components/FAQs/FAQs';
+import Footer from './components/Footer/Footer';
+
 
 const App = () => {
   return (
-   <>
-   <Navbar/>
-   <Header/>
-   <Products/>
-   <Company/>
-   <FAQs/>
-   <Footer/>
-   </>
+    <>
+      <header>
+        <Navbar />
+      </header>
+      <main>
+        <Home />
+        <Products />
+        <Company />
+        <FAQs />
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </>
+  );
+};
 
-    
-  )
-}
-
-export default App
+export default App;
