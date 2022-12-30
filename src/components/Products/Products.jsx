d62b65406fdc80ce0e354016dd6f5930ad0f8e16
PamelaKlapp <pameklapp@gmail.com>
2022-12-30 12:02:36 +0100

    Update Products.jsx

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,35 +1,17 @@
 import React from 'react';
 import DisplayProducts from '../DisplayProducts/DisplayProducts';
-import Product from '../Product/Product';
 import './Products.css';
-
 
 const Products = () => {
   return (
-    <main id='krawatte' className="products-container container">
-      <section className="products">
-
-        <div className="products-intro">
-          <div></div>
-          <div className="products_text">
-            <h2>Recycling - die neue Mode</h2>
-            <p>
-              Die Krawatten werden aus nachhaltigen Rohstoffen wie Bio-Baumwolle
-              und Abfällen hergestellt. Wir arbeiten auch mit kleinen
-              Familienbetrieben zusammen, die alles in Chile herstellen. Sie
-              werden aus Textilabfällen hergestellt, die wiederverwertet werden,
-              um ihnen ein zweites Leben zu geben. Sie sind also nicht nur
-              einzigartig, sondern auch nachhaltig. Sie werden von lokalen
-              Näherinnen mit exzellenter Handwerkskunst in Handarbeit
-              hergestellt.
-            </p>
-          </div>
-          <div></div>
+    <section id="products" className="container">
+      <div className="products-container">
+        <h2>Recycling - New Collection</h2>
+        <div className="product-display">
+          <DisplayProducts />
         </div>
-      <Product />
-      <DisplayProducts/>
-      </section>
-    </main>
+      </div>
+    </section>
   );
 };
 
