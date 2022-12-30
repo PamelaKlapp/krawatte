82e097dfca765de7ce3872bc31e0a5763ae8515d
PamelaKlapp <pameklapp@gmail.com>
2022-12-30 12:00:54 +0100

    Create data.js

---
+++ src/data.js
@@ -0,0 +1,96 @@
+const data = {
+
+    products: [
+        {
+            name: 'Toto',
+            slug: 'toto-kwt',
+            image: '/images/krawatte7.png',
+            price: 24.95,
+            description: 'Die perfekte Krawatte, um aufzufallen und der Mittelpunkt der Party zu sein.',
+        },
+        {
+            name: 'Le Wine',
+            slug: 'lewine-kwt',
+            image: 'images/krawatte2.png',
+            price: 24.95,
+            description: 'blabla',
+        },
+        {
+            name: 'Dorothy',
+            slug: 'lewine-kwt1',
+            image: 'images/krawatte7.png',
+            price: 24.95,
+            description: 'blabla',
+        },
+        {
+            name: 'Flour',
+            slug: 'lewine-kwt2',
+            image: 'images/krawatte8.png',
+            price: 24.95,
+            description: 'blabla',
+        },
+        {
+            name: 'Renny',
+            slug: 'lewine-kwt3',
+            image: 'images/krawatte9.png',
+            price: 24.95,
+            description: 'blabla',
+        },
+        {
+            name: 'Nannu',
+            slug: 'lewine-kwt4',
+            image: 'images/krawatte10.png',
+            price: 24.95,
+            description: 'blabla',
+        },
+        {
+            name: 'Blue',
+            slug: 'lewine-kwt5',
+            image: 'images/krawatte11.png',
+            price: 24.95,
+            description: 'blabla',
+        },
+
+    ],
+
+    faq:[
+        {
+            number: 1,
+            question: 'Wo werden die Krawatten hergestellt?',
+            answer: 'Die Krawatten werden von chilenischen Näherinnen handgefertigt.',
+        },
+        {
+            number: 2,
+            question: 'Sind alle Designs einzigartig?',
+            answer: 'Wir sind sehr stolz darauf, sagen zu können: Ja, jeder Entwurf ist einzigartig.',
+        },
+        {
+            number: 3,
+            question: 'Liefern Sie auch außerhalb Chiles?',
+            answer: 'Ja, wir versenden weltweit.',
+        },
+        {
+            number: 4,
+            question: 'Wie lange dauert der Versand?',
+            answer: 'Der Versand dauert zwischen 2-4 Arbeitstagen.',
+        },
+        {
+            number: 5,
+            question: 'Welche Zahlungsmöglichkeiten gibt es?',
+            answer: 'Im Moment können Sie nur mit PayPal bezahlen.',
+        },
+        {
+            number: 6,
+            question: 'Ist es möglich, das Produkt zurückzusenden?',
+            answer: 'Ja, aber Sie müssen die Versandkosten selbst tragen.',
+        },
+        {
+            number: 7,
+            question: 'Wie kann man sie am besten waschen?',
+            answer: 'Wir empfehlen, sie von Hand in lauwarmem Wasser zu waschen.',
+        },
+    ]
+
+};
+
+export default data;
