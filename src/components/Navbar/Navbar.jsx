a4858ccd6406c15f3f85f4d427652a9357bf7052
PamelaKlapp <pameklapp@gmail.com>
2023-01-05 16:59:44 +0100

    add useState

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,38 +1,40 @@
 import React from 'react';
-import { CiSearch } from 'react-icons/ci';
+import { CiUser } from 'react-icons/ci';
 import { CiShoppingCart } from 'react-icons/ci';
 import { BiMenu } from 'react-icons/bi';
 import { AiOutlineClose } from 'react-icons/ai';
 import './Navbar.css'
+import { useState } from 'react';
 
 const Navbar = () => {
+  const [activeNav, setActiveNav] = useState('#home')
   return (
     <nav>
-      <div className="nav_container container">
+      <div className="nav-container container">
         
         <a href="#home" className='logo'>KRAWATTE</a>
 
         <ul>
           <li>
-            <a href="#home" className='active'>Home</a>
+            <a href="#home" onClick={() => setActiveNav('#home')} className={activeNav === "#home" ? 'active' : ' '}>Home</a>
           </li>
           <li>
-            <a href="#products">Krawatten</a>
+            <a href="#products" onClick={() => setActiveNav('#products')} className={activeNav === "#products" ? 'active' : ' '}>Krawatten</a>
           </li>
           <li>
-            <a href="#company">Über uns</a>
+            <a href="#company" onClick={() => setActiveNav('#company')} className={activeNav === "#company" ? 'active' : ' '}>Über uns</a>
           </li>
           <li>
-            <a href="#faq">FAQs</a>
+            <a href="#faq" onClick={() => setActiveNav('#faq')} className={activeNav === "#faq" ? 'active' : ' '}>FAQs</a>
           </li>
           <li>
-            <a href="#contact">Kontakt</a>
+            <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === "#contact" ? 'active' : ' '}>Kontakt</a>
           </li>
         </ul>
 
         <div className="icons">
           <a href="">
-            <CiSearch />
+            <CiUser />
           </a>
           <a href="">
             <CiShoppingCart />
